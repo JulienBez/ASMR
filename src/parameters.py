26bc6548182d8c544c9702e3548e264785f4d1ef
JulienBez <julienbezancon@gmail.com>
2025-04-25 12:05:02 +0200

    README file

--- src/parameters.py
+++ src/parameters.py
@@ -1,5 +1,5 @@
 # RUN ASMR ON <NAMEPATH> #
-NAMEPATH = "FrUIT"            # corpus we work on (must have a folder and a json file with this name)
+NAMEPATH = "FrUIT"                  # corpus we work on (must have a folder and a json file with this name)
 
 
 # PARAMETERS #
